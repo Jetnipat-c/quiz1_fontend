db9341dc5920b6b6cd146754b5b570cadaa54e3b
alonecheer <alonecheer@hotmail.com>
2021-03-21 00:46:57 +0700

    :tada: map

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,45 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import styles from "../styles/Home.module.css";
+const URL = "http://localhost:6969/api/todo";
 
-export default function Home() {
+const index = () => {
+  const [todo, setTodo] = useState({});
+  console.log(todo);
+
+  useEffect(() => {
+    getTodo();
+  }, []);
+
+  const getTodo = async () => {
+    let result = await axios.get(URL);
+    setTodo(result.data.list);
+  };
+
+  const showTodo = () => {
+    if (todo && todo.length) {
+      return todo.map((item, index) => {
+        return (
+          <li>
+            <b>Title :</b> {item.title} <b>Status :</b> {item.status}
+            <button className={styles.button}>Get</button>
+            <button className={styles.button}>Update</button>
+            <button className={styles.button}>Delete</button>
+          </li>
+        );
+      });
+    } else {
+      return <p>Loading...</p>;
+    }
+  };
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <h2>Todo list</h2>
+      <div>{showTodo()}</div>
+      <h2>Add todo</h2>
+      Title: <input type="text" name="title"></input>
+      status: <input type="text" name="status"></input>
     </div>
-  )
-}
+  );
+};
+export default index;
